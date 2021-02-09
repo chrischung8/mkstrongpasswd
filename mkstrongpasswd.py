77bf254c2f4c29ad86863434c47c9ea39620cff2
Chris Chung <55639654+chrischung8@users.noreply.github.com>
2021-02-09 13:06:08 +0000

    Add files via upload
    Cleaned up the code a bit and added a function to generate words based on user input.

--- mkstrongpasswd.py
+++ mkstrongpasswd.py
@@ -1,8 +1,30 @@
 import random
 import string
+import pyperclip
+
 from random_words import RandomWords
 
+print('''
+Password Generator 
+==================
+''')
 
+rw = RandomWords()
+count = int(input("How many words should the password contain? "))
+
+
+# Function to randomly generate a list of random words
+def generate_words(count):
+    result = []
+    for i in range(count):
+        result += rw.random_words()
+    return result
+
+
+words = (generate_words(count))
+
+
+# Function to apply upper and lower case to letters in a random sequence
 def random_case(word):
     result = ''
     for char in word:
@@ -14,22 +36,18 @@ def random_case(word):
     return result
 
 
-rw = RandomWords()
-word1 = rw.random_words()
-word2 = rw.random_words()
-word3 = rw.random_words()
-word4 = rw.random_words()
-word5 = rw.random_words()
-
-sep = ['-']
-num = random.randint(0, 9)
+number = random.randint(0, 9)
 punctuation = string.punctuation
 str_to_list = list(punctuation)
-special_char = random.choice(str_to_list)
+special_character = random.choice(str_to_list)
 
-list1 = word1 + sep + word2 + sep + word3 + sep + word4 + sep + word5
-passwd = ''.join(list1)
-mkpasswd = passwd + str(num) + special_char
+# Join all items in the list to a string, separated by a separator
+passwd = '-'.join(words)
 
-print(f"Password generated: {random_case(mkpasswd)}")
-print(f"Password length: {len(mkpasswd)}")
+password = passwd + str(number) + special_character
+xkcd_password = random_case(password)
+
+print(f"Password generated: {xkcd_password}")
+print(f"Password length: {len(password)}")
+pyperclip.copy(xkcd_password)
+print("Password copied to clipboard.")
